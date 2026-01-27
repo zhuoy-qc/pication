7d5153aadafe6ec56ee9d8942610f137033fb6a1
zhuoyin <149404657+zhuoy-qc@users.noreply.github.com>
2026-01-27 20:00:01 +0800

    Update train.py

--- non-ARG_energy_prediction_model/train.py
+++ non-ARG_energy_prediction_model/train.py
@@ -1,3 +1,5 @@
+#As this training code did not save the optuna randomness search parameters, output pkl could be different
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
